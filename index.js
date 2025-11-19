56be3aa5edc2e58951b201628a8198ee6f2497c6
skghosh87 <sarghosh@gmail.com>
2025-11-19 23:09:08 +0600

    Backend Start Now

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const port = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
