0bc42ff51f07c53545b2b45c13faff66a6bfdddf
skghosh87 <sarghosh@gmail.com>
2025-11-21 19:14:48 +0600

    Mongodb added

--- index.js
+++ index.js
@@ -4,6 +4,35 @@ const port = 3000;
 
 const app = express();
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://socialdevelopment:ob048QdAZa3GnSHX@skghosh.wrzjkjg.mongodb.net/?appName=Skghosh";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
